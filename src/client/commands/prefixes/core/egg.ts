5d40d1482149cca2a0fc38db935139d514e69888
mynameisbob1928 <129603125+mynameisbob1928@users.noreply.github.com>
2025-07-24 11:52:09 +0100

    Update egg.ts

--- src/client/commands/prefixes/core/egg.ts
+++ src/client/commands/prefixes/core/egg.ts
@@ -3,6 +3,7 @@ import Prefix from 'bases/prefix';
 export default new Prefix({
   name: 'egg',
   description: 'egg',
+  category: 'core',
   cooldown: 0,
   // cache: true,
   async run(client, message, args) {
